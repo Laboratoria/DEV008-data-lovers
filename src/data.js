ef9049114493f4f76b88650bb5a97de1a2e33d5c
JB-Gra <98495762+JB-Gra@users.noreply.github.com>
2023-05-23 09:08:50 -0600

    Modificaciones en los comentarios para mejorar la legibilidad

--- src/data.js
+++ src/data.js
@@ -1,21 +1,14 @@
 import ghibliData from './data/ghibli/ghibli.js'; 
-//obtenemos la base de datos y la llamamos como un objeto ghibliData.
+// Obtenemos la base de datos y la llamamos como un objeto ghibliData.
 export const getFilms = () => { 
-  /* devuelve la lista de películas del objeto ghibliData.*/
+  // Devuelve la lista de películas del objeto ghibliData.
   return ghibliData.films;
 };
-/*estas funciones son de ejemplo
-
-export const database = () => {
-  return 'example';
-};
-
-<<<<<<< HEAD
-// export const anotherExample = () => {
-//  return 'OMG';
+// Estas funciones son de ejemplo
+// export const database = () => {
+//  return 'example';
 // };
-=======
-export const anotherExample = () => {
-  return 'OMG';
-};*/
->>>>>>> origin
+
+// export const anotherExample = () => {
+//   return 'OMG';
+// };
--- src/main.js
+++ src/main.js
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-import { database } from './data.js';
-import data from './data/ghibli/ghibli.js';
-// import data from './data/lol/lol.js';
-// import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
-
-console.log(database, data);
-=======
 import { getFilms } from './data.js';
 
 const films = getFilms();
 console.log(films);
 const filmsContainer = document.getElementById('films-container');
-const filmList = films.map(film => { //para que para todas las peliculas me retorne lo mismo, en este caso es el titulo, la foto y el año
+const filmList = films.map(film => { // Para que para todas las peliculas me retorne lo mismo, en este caso es el titulo, la foto y el año
     return `
       <div>
         <h2>${film.title}</h2>
@@ -20,8 +11,7 @@ const filmList = films.map(film => { //para que para todas las peliculas me reto
         <p>${film.release_date}</p>
         </div>
         `;
-      }).join(''); //el resultado de .map() es un array de fragmentos HTML. Para mostrarlos correctamente en la interfaz, necesitamos combinarlos en una sola cadena de texto. Es aquí donde entra en juego el método .join('').
+      }).join(''); // El resultado de .map() es un array de fragmentos HTML. Para mostrarlos correctamente en la interfaz, necesitamos combinarlos en una sola cadena de texto. Es aquí donde entra en juego el método .join('').
     
       // Agrega el contenido HTML al elemento <div>
-    filmsContainer.innerHTML = filmList; //para meterle al div la lista que hicimos.
->>>>>>> origin
+    filmsContainer.innerHTML = filmList; // Para meterle al div la lista que hicimos.
