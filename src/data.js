df7c06aafb46fb444d1d0b75c5bf7887b33d0fc7
JB-Gra <98495762+JB-Gra@users.noreply.github.com>
2023-05-23 08:54:54 -0600

    Tratando de recuperar archivos

--- src/data.js
+++ src/data.js
@@ -1,9 +1,21 @@
-// estas funciones son de ejemplo
+import ghibliData from './data/ghibli/ghibli.js'; 
+//obtenemos la base de datos y la llamamos como un objeto ghibliData.
+export const getFilms = () => { 
+  /* devuelve la lista de películas del objeto ghibliData.*/
+  return ghibliData.films;
+};
+/*estas funciones son de ejemplo
 
 export const database = () => {
   return 'example';
 };
 
+<<<<<<< HEAD
 // export const anotherExample = () => {
 //  return 'OMG';
 // };
+=======
+export const anotherExample = () => {
+  return 'OMG';
+};*/
+>>>>>>> origin
--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import { database } from './data.js';
 import data from './data/ghibli/ghibli.js';
 // import data from './data/lol/lol.js';
@@ -5,3 +6,22 @@ import data from './data/ghibli/ghibli.js';
 // import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(database, data);
+=======
+import { getFilms } from './data.js';
+
+const films = getFilms();
+console.log(films);
+const filmsContainer = document.getElementById('films-container');
+const filmList = films.map(film => { //para que para todas las peliculas me retorne lo mismo, en este caso es el titulo, la foto y el año
+    return `
+      <div>
+        <h2>${film.title}</h2>
+        <img src="${film.poster}" alt="${film.title} Poster">
+        <p>${film.release_date}</p>
+        </div>
+        `;
+      }).join(''); //el resultado de .map() es un array de fragmentos HTML. Para mostrarlos correctamente en la interfaz, necesitamos combinarlos en una sola cadena de texto. Es aquí donde entra en juego el método .join('').
+    
+      // Agrega el contenido HTML al elemento <div>
+    filmsContainer.innerHTML = filmList; //para meterle al div la lista que hicimos.
+>>>>>>> origin
