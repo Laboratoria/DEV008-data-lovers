23027e355e0e9efb974b2791ee0bf043d6e4e1c9
RoxiFOchoaG <46870050+roxifochoag@users.noreply.github.com>
2023-05-24 09:46:40 -0500

    Agregando los cambios realizados por Katty: Modificación del footer y index

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,10 @@
+
+
+
+
 import { example } from '../data.js';
 // import data from './data/lol/lol.js';
-import data from '../data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+
 
 console.log(example, data);
 
@@ -11,4 +14,3 @@ console.log(example, data);
 
 
 
-/**TESTIMONIOS**/
