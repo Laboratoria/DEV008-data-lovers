ab073dbabfa7cf282c0e9c231d53135aba2bcc13
agilema <apaomendezalcantara@gmail.com>
2023-05-30 14:13:41 -0600

    pagina principal

---
+++ .history/src/initial.ghibli/main_20230530113750.js
@@ -0,0 +1,6 @@
+import { example } from './data.js';
+// import data from './data/lol/lol.js';
+import data from '../data/';
+// import data from './data/rickandmorty/rickandmorty.js';
+
+console.log(example, data);
