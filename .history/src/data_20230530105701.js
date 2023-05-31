89de9c701279e37c32554ad92a80892e315313e9
agilema <apaomendezalcantara@gmail.com>
2023-05-31 11:42:06 -0600

    Enlace de la pag1 a la pag2

--- .history/src/data_20230530105701.js
+++ .history/src/data_20230530105701.js
@@ -1,4 +1,5 @@
 // estas funciones son de ejemplo
+
 export const example = () => {
   return 'example';
 };
--- .history/src/data_20230530194417.js
+++ .history/src/data_20230530194417.js
@@ -6,3 +6,4 @@ export const example = () => {
 export const anotherExample = () => {
   return 'OMG';
 };
+}
